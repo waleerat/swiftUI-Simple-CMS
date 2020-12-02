dd61ac5a5c64ebafe89ea47427d768940d5028d2
Waleerat S. Gottlieb <waleerat.gottlieb@icloud.com>
2020-12-02 12:09:16 +0100

    Initial Commit

---
+++ swiftUI-Simple-CMS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUI-Simple-CMS
+//
+//  Created by Waleerat Gottlieb on 2020-12-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
